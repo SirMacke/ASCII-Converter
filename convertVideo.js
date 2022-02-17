2d4e5d1792b9cf33db7c4001418ea6699d5b1008
Maximilian <max.helmersson@gmail.com>
2022-02-17 22:39:27 +0100

    restructured

---
+++ convertVideo.js
@@ -0,0 +1,18 @@
+const fs = require('fs');
+const Jimp = require('jimp');
+const { cobRes, covertToASCII, delay } = require('./helperFunctions');
+
+module.exports = async (variables) => {
+  if (!fs.existsSync(`./images/${variables.imageFileName}.png`)) {
+    Jimp.read(`./images/${variables.imageFileName}.jpg`, function (err, image) {
+      if (err) console.log(err);
+      else image.write(`./images/${variables.imageFileName}.png`);
+    });
+    await delay(1000);
+  }
+
+  await cobRes(fs.readFileSync(`./images/${variables.imageFileName}.png`), variables.asciiHeight, buffer => fs.writeFileSync(`./images/${variables.imageFileName}2.png`, buffer));
+  await delay(500);
+
+  await covertToASCII(fs.readFileSync(`./images/${variables.imageFileName}2.png`), variables.grayScaleBias, variables.darkOrLight);
+}
